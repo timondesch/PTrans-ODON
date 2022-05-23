b7b26a3fbe310c18bb3381ac1a6faeb9222618d9
E20A805D <timon.deschamps@etu.univ-nantes.fr>
2022-05-23 14:17:58 +0200

    added gitignore and files related to inpainting DL

---
+++ tools/IPDL/inference.py
@@ -0,0 +1,68 @@
+import tensorflow as tf
+print(tf.__version__)
+from tensorflow import keras
+
+import matplotlib.pyplot as plt
+
+import cv2
+import PIL
+import numpy as np
+from skimage.color import rgba2rgb
+
+class inpaintingModel:
+  '''
+  Build UNET like model for image inpaining task.
+  '''
+  def prepare_model(self, input_size=(512, 1024,1)):
+    inputs = keras.layers.Input(input_size)
+
+    conv1, pool1 = self.__ConvBlock(32, (3,3), (2,2), 'relu', 'same', inputs) 
+    conv2, pool2 = self.__ConvBlock(64, (3,3), (2,2), 'relu', 'same', pool1)
+    conv3, pool3 = self.__ConvBlock(128, (3,3), (2,2), 'relu', 'same', pool2) 
+    conv4, pool4 = self.__ConvBlock(256, (3,3), (2,2), 'relu', 'same', pool3) 
+    
+    conv5, up6 = self.__UpConvBlock(512, 256, (3,3), (2,2), (2,2), 'relu', 'same', pool4, conv4)
+    conv6, up7 = self.__UpConvBlock(256, 128, (3,3), (2,2), (2,2), 'relu', 'same', up6, conv3)
+    conv7, up8 = self.__UpConvBlock(128, 64, (3,3), (2,2), (2,2), 'relu', 'same', up7, conv2)
+    conv8, up9 = self.__UpConvBlock(64, 32, (3,3), (2,2), (2,2), 'relu', 'same', up8, conv1)
+    
+    conv9 = self.__ConvBlock(32, (3,3), (2,2), 'relu', 'same', up9, False)
+    
+    outputs = keras.layers.Conv2D(1, (3, 3), activation='sigmoid', padding='same')(conv9)
+
+    return keras.models.Model(inputs=[inputs], outputs=[outputs])  
+
+  def __ConvBlock(self, filters, kernel_size, pool_size, activation, padding, connecting_layer, pool_layer=True):
+    conv = keras.layers.Conv2D(filters=filters, kernel_size=kernel_size, activation=activation, padding=padding)(connecting_layer)
+    conv = keras.layers.Conv2D(filters=filters, kernel_size=kernel_size, activation=activation, padding=padding)(conv)
+    if pool_layer:
+      pool = keras.layers.MaxPooling2D(pool_size)(conv)
+      return conv, pool
+    else:
+      return conv
+
+  def __UpConvBlock(self, filters, up_filters, kernel_size, up_kernel, up_stride, activation, padding, connecting_layer, shared_layer):
+    conv = keras.layers.Conv2D(filters=filters, kernel_size=kernel_size, activation=activation, padding=padding)(connecting_layer)
+    conv = keras.layers.Conv2D(filters=filters, kernel_size=kernel_size, activation=activation, padding=padding)(conv)
+    up = keras.layers.Conv2DTranspose(filters=up_filters, kernel_size=up_kernel, strides=up_stride, padding=padding)(conv)
+    up = keras.layers.concatenate([up, shared_layer], axis=3)
+
+    return conv, up
+
+def open_resize(path):
+    return np.array((PIL.Image.open(path)).convert('L').resize((1024, 512)))/255
+
+def show(img):
+    cv2.imshow(img*255)
+
+def createModel():
+    keras.backend.clear_session()
+    model = inpaintingModel().prepare_model()
+    model.compile(optimizer='adam', loss='mean_absolute_error')
+    # keras.utils.plot_model(model, show_shapes=True, dpi=76, to_file='model_v1.png')
+    model.load_weights("./model_weights.keras")
+    return model
+
+def infer(model, img):
+  inpainted_image = model.predict(img.reshape((1,)+img.shape))
+  return inpainted_image.reshape(inpainted_image.shape[1:-1])
